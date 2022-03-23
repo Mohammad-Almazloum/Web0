86a1663324f8ccb26662940342be92f8bb82c8a2
Your Name <you@example.com>
2022-03-23 09:34:30 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+fetch('http://localhost:3000/Data')
+  .then(response => response.json())
+  .then(json => {
+      var RH=document.getElementById('RH')
+      var originalContent=`<div>
+      <div class="icon"><i class="mangala"></i></div>
+      <h4 class="title"><a href="">ABC</a></h4>
+      <p class="description">EFG</p>
+    </div>`
+      for(i=0;i<json.length;i++){
+
+          var myContent=document.createElement('div');
+          customizedContent=originalContent;
+          customizedContent=customizedContent.replace('ABC',json[i].title)
+          customizedContent=customizedContent.replace('EFG',json[i].description)
+          customizedContent=customizedContent.replace('mangala',json[i].icon)
+          myContent.className='col-lg-4 col-md-6 icon-box'
+          myContent.innerHTML=customizedContent;
+          RH.appendChild(myContent);
+
+
+      }
+
+  })
